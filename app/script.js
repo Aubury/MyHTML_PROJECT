625f9c572ea55bb385d1fee618c1e320a342f5fc
Aubury <32746247+Aubury@users.noreply.github.com>
2019-04-12 18:06:03 +0300

    plan

--- app/script.js
+++ app/script.js
@@ -2,15 +2,34 @@ let body = document.querySelector("body");
 
 document.querySelector('.infButton').addEventListener('click',function(){
 
-    let plan = document.querySelector('.sliderItem').firstElementChild.children[1];
+    let plan = document.querySelector('.sliderItem').firstElementChild.firstElementChild,
+        img = document.querySelectorAll('.action');
+
     
     if(plan.style.display == 'none'){
-       
+        
+        
+        if(window.screen.width <= 1050){
+
+            for(let i = 0; i < img.length; i++){
+
+                img[i].style.display = 'none';
+            }
+          
+        }
+        
         plan.style.display = 'block';
-    
     }else{
 
         plan.style.display = 'none';
+        
+        if(window.screen.width <= 1050){
+           
+            for(let i = 0; i < img.length; i++){
+
+                img[i].style.display = 'block';
+            }
+      } 
     }
 
  
