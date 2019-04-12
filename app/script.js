fb27791a37704d2b9cc03bcb3b0c46bced170eca
Aubury <32746247+Aubury@users.noreply.github.com>
2019-04-12 15:37:06 +0300

    pages

--- app/script.js
+++ app/script.js
@@ -2,7 +2,7 @@ let body = document.querySelector("body");
 
 document.querySelector('.infButton').addEventListener('click',function(){
 
-    let plan = document.querySelector('.top').firstElementChild.children[1];
+    let plan = document.querySelector('.sliderItem').firstElementChild.children[1];
     
     if(plan.style.display == 'none'){
        
@@ -16,51 +16,4 @@ document.querySelector('.infButton').addEventListener('click',function(){
  
 });
 
-function getElementAttrs(el) {
-    return [].slice.call(el.attributes).map((attr) => {
-      return {
-        name: attr.name,
-        value: attr.value
-      }
-    });
-  }
-  function removeElementAttrs(el) {
-    return [].slice.call(el.attributes).map((attr) => {
-       
-       return delete(attr);
-      
-    });
-  }
 
-document.querySelector('.topButton').addEventListener('click',function(){
-
-    let firstChild = document.querySelector('.top').firstElementChild,
-        secondChild = document.querySelector('.imgHouses').firstElementChild.nextElementSibling.nextElementSibling,
-        lastChild = document.querySelector('.imgHouses').lastElementChild.previousElementSibling;
-        
-        
-       let cloneFirst = '';
-
-      for(let i = 0; i< firstChild.lenght; i++)
-      {
-          cloneFirst[i].innerHTML = firstChild[i].innerHTML;
-      }
-      
-      removeElementAttrs(cloneFirst);
-
-        let attrFirstChild = getElementAttrs(lastChild);
-
-        for(let i = 0; i < attrFirstChild.lenght; i++){
-
-            cloneFirst.setAttribute(attrFirstChild[i].name,attrFirstChild[i].value);
-        }
-        
-        lastChild.replaceWith(cloneFirst); 
-       
-        // document.querySelector('.top').firstElementChild.replaceWith(secondChild);
-        
-       
-        
-
-
-});
